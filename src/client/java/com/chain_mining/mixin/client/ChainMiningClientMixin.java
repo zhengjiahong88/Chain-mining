bb6a807e761db248caceb365c4226a198f1cc328
zhengjiahong88 <149258317+zhengjiahong88@users.noreply.github.com>
2026-01-28 16:47:04 +0800

    Example mod

--- src/client/java/com/chain_mining/mixin/client/ChainMiningClientMixin.java
+++ src/client/java/com/chain_mining/mixin/client/ChainMiningClientMixin.java
@@ -1,13 +1,13 @@
-package com.example.mixin.client;
+package com.chain_mining.mixin.client;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
-public class ExampleClientMixin {
+@Mixin(MinecraftClient.class)
+public class ChainMiningClientMixin {
 	@Inject(at = @At("HEAD"), method = "run")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of Minecraft.run()V
--- src/main/java/com/chain_mining/ChainMiningMod.java
+++ src/main/java/com/chain_mining/ChainMiningMod.java
@@ -1,12 +1,12 @@
-package com.example;
+package com.chain_mining;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class ChainMiningMod implements ModInitializer {
+	public static final String MOD_ID = "chain_mining";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
